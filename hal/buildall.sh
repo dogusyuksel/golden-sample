a26e381383650c5c9d33afd6b8ff638d0bb242a0
dogusyuksel <dogusyuk@gmail.com>
2024-04-11 17:09:33 +0100

    remove hal auto build for now

--- hal/buildall.sh
+++ hal/buildall.sh
@@ -5,9 +5,10 @@ set -e
 files=$(find . -name '*' -type d -maxdepth 1 -mindepth 1)
 while IFS= read -r line; do
     dirname=$(basename "$line")
-    cmake $dirname/ -B $dirname/
-    make -C $dirname clean
-    make -C $dirname all
+    mkdir  $dirname/build
+    cmake $dirname/ -B $dirname/build/
+    make -C $dirname/build clean
+    make -C $dirname/build all
     cd ..
 done <<< "$files"
 
