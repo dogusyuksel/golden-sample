347e58ffd5bc18283a3ef9a97b5bd7d9c26cafa2
dogusyuksel <dogusyuk@gmail.com>
2024-04-12 16:31:03 +0100

    rtos added to gpio hal example (#8)

--- hal/GPIO_Out/src/main.c
+++ hal/GPIO_Out/src/main.c
@@ -1,61 +1,34 @@
-/* USER CODE BEGIN Header */
-/**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2024 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+void toggle_a(void *data) {
+    (void)data;
+    while (1) {
+        HAL_GPIO_TogglePin(GPIOB, SENSE_LED_A_Pin);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
-/* USER CODE END Includes */
+void toggle_b(void *data) {
+    (void)data;
+    while (1) {
+        HAL_GPIO_TogglePin(GPIOB, SENSE_LED_B_Pin);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
+void toggle_c(void *data) {
+    (void)data;
+    while (1) {
+        HAL_GPIO_TogglePin(GPIOB, SENSE_LED_C_Pin);
+        vTaskDelay(pdMS_TO_TICKS(3000));
+    }
+}
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 /**
  * @brief  The application entry point.
@@ -63,40 +36,23 @@ static void MX_GPIO_Init(void);
  */
 int main(void) {
 
-    /* USER CODE BEGIN 1 */
-
-    /* USER CODE END 1 */
-
-    /* MCU Configuration--------------------------------------------------------*/
-
     /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
     HAL_Init();
-
-    /* USER CODE BEGIN Init */
-
-    /* USER CODE END Init */
 
     /* Configure the system clock */
     SystemClock_Config();
 
-    /* USER CODE BEGIN SysInit */
-
-    /* USER CODE END SysInit */
-
     /* Initialize all configured peripherals */
     MX_GPIO_Init();
-    /* USER CODE BEGIN 2 */
 
-    /* USER CODE END 2 */
+    xTaskCreate(toggle_a, "toggle_a", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(toggle_b, "toggle_b", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(toggle_c, "toggle_c", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
 
+    vTaskStartScheduler();
     /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
     while (1) {
-        /* USER CODE END WHILE */
-
-        /* USER CODE BEGIN 3 */
     }
-    /* USER CODE END 3 */
 }
 
 /**
