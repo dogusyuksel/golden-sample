cbe13789351c70f748943be934faf232f5758732
dogusyuksel <dogusyuk@gmail.com>
2024-04-12 10:38:48 +0100

    made HAL downloadable in runtime

--- hal/buildall.sh
+++ hal/buildall.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git clone https://github.com/STMicroelectronics/stm32f1xx_hal_driver.git ../thirdparty/STM32F1XX/Drivers/STM32F1xx_HAL_Driver
+
 files=$(find . -name '*' -type d -maxdepth 1 -mindepth 1)
 while IFS= read -r line; do
     dirname=$(basename "$line")
