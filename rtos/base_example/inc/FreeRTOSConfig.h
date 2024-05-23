002ce21573ebdd9093c5566078a6be75ecb21fbc
dogusyuksel <dogusyuk@gmail.com>
2024-05-23 16:23:31 +0100

    rtos notify and mutex added

--- rtos/base_example/inc/FreeRTOSConfig.h
+++ rtos/base_example/inc/FreeRTOSConfig.h
@@ -85,6 +85,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil 1
 #define INCLUDE_vTaskDelay 1
 #define INCLUDE_xTaskGetHandle 1
+#define configUSE_TASK_NOTIFICATIONS 1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
