828a2a0c2d01710a1d270a8d7c338c9ae3bee84e
dogusyuksel <dogusyuk@gmail.com>
2024-07-28 22:59:47 +0100

    remove libcanard second dependency (#41)

--- hw_tools/download_repos.sh
+++ hw_tools/download_repos.sh
@@ -2,11 +2,6 @@
 
 if [ ! -d "../thirdparty/STM32F103X_HAL" ]; then
     git clone https://github.com/STMicroelectronics/stm32f1xx_hal_driver.git ../thirdparty/STM32F103X_HAL
-    files=$(find ../thirdparty/STM32F103X_HAL -name '*template.c' -type f)
-    while IFS= read -r line; do
-        echo "deleted file $line"
-        rm -rf $line
-    done <<< "$files"
 fi
 
 if [ ! -d "../thirdparty/cmsis-header-stm32" ]; then
@@ -26,12 +21,13 @@ if [ ! -d "../thirdparty/openocd" ]; then
 fi
 
 if [ ! -d "CANBUS/libcanard" ]; then
-    git clone https://github.com/skyyuzhang/libcanard.git CANBUS/libcanard
+    git clone https://github.com/OpenCyphal/libcanard.git CANBUS/libcanard
     cd CANBUS/libcanard
-    git checkout c3f77b2354980348c74289557614a53f6d560482
+    git checkout 43de1c4966b8d1e5d57978949d63e697f045b358
     git submodule update --init --recursive
     cd -
     patch -d CANBUS -p1 < ./CANBUS/transport.py.patch
+    patch -d CANBUS -p1 < ./CANBUS/canard_stm32.patch
     ./CANBUS/libcanard/dsdl_compiler/libcanard_dsdlc ./CANBUS/nodes --incdir ./CANBUS/libcanard/dsdl_compiler/pyuavcan/uavcan --outdir ./CANBUS/generated_files
 fi
 
--- rtos/download_repos.sh
+++ rtos/download_repos.sh
@@ -2,11 +2,6 @@
 
 if [ ! -d "../thirdparty/STM32L4XX_HAL" ]; then
     git clone https://github.com/STMicroelectronics/stm32l4xx_hal_driver.git ../thirdparty/STM32L4XX_HAL
-    files=$(find ../thirdparty/STM32L4XX_HAL -name '*template.c' -type f)
-    while IFS= read -r line; do
-        echo "deleted file $line"
-        rm -rf $line
-    done <<< "$files"
 fi
 
 if [ ! -d "../thirdparty/cmsis-header-stm32" ]; then
