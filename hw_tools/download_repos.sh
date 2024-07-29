e2d3f77b55d6044569e3b438d144446bfe7cc1cb
dogusyuksel <dogusyuk@gmail.com>
2024-07-29 21:44:26 +0100

    added stm32f`03 hal submodule

--- hw_tools/download_repos.sh
+++ hw_tools/download_repos.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ ! -d "../thirdparty/STM32F103X_HAL" ]; then
-    git clone https://github.com/STMicroelectronics/stm32f1xx_hal_driver.git ../thirdparty/STM32F103X_HAL
-fi
-
 if [ ! -d "../thirdparty/cmsis-header-stm32" ]; then
     git clone https://github.com/modm-io/cmsis-header-stm32.git ../thirdparty/cmsis-header-stm32
 fi
