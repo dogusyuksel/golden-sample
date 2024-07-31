651f2fe49ca921ea872d4a2f52c84ca9de7a09fd
dogusyuksel <dogusyuk@gmail.com>
2024-07-31 01:25:40 +0100

    remove ceedling

--- ceedling/myproject/src/customprintrandom.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CUSRANDPRINT_
-#define __CUSRANDPRINT_
-
-#include <stdbool.h>
-
-bool random_print(const char *str);
-
-#endif //
--- ceedling/thirdpartycode/customprint.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CUSTOMPRINT_
-#define __CUSTOMPRINT_
-
-#include <stdbool.h>
-
-bool custom_print(const char *str);
-
-#endif //__CUSTOMPRINT_
